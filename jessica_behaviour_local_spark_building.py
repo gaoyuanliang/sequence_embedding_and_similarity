2d99da541f00d38410b2a2e473019f8dd4a91e8e
Jessica (Gaoyuan Liang) <gaoyuanliang@outlook.com>
2020-07-17 13:21:14 +0400

    Create jessica_behaviour_local_spark_building.py

---
+++ jessica_behaviour_local_spark_building.py
@@ -0,0 +1,9 @@
+########jessica_behaviour_local_spark_building.py########
+from pyspark import *
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+sc = SparkContext("local")
+sqlContext = SparkSession.builder.getOrCreate()
+########jessica_behaviour_local_spark_building.py########
