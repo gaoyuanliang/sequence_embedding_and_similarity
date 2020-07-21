bce7f1bf6ec47465caf31b1252efc5e724bf9f0b
Jessica (Gaoyuan Liang) <gaoyuanliang@outlook.com>
2020-07-21 15:30:56 +0400

    Update and rename jessica_behaviour_local_spark_building.py to jessica_koktail_local_spark_building.py

--- jessica_koktail_local_spark_building.py
+++ jessica_koktail_local_spark_building.py
@@ -1,4 +1,4 @@
-########jessica_behaviour_local_spark_building.py########
+########jessica_koktail_local_spark_building.py########
 from pyspark import *
 from pyspark.sql import *
 from pyspark.sql.types import *
@@ -6,4 +6,4 @@ from pyspark.sql.functions import *
 
 sc = SparkContext("local")
 sqlContext = SparkSession.builder.getOrCreate()
-########jessica_behaviour_local_spark_building.py########
+########jessica_koktail_local_spark_building.py########
